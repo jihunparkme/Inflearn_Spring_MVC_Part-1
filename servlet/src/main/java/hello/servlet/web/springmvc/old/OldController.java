8be327677af21ca9f9ff8c0c099b74f79145c984
jihunparkme <jihunpark.tech@gmail.com>
2021-08-24 23:08:45 +0900

    [MVC 프레임워크 만들기] 핸들러 매핑과 핸들러 어댑터

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// Spring Bean 이름으로 핸들러를 찾는다. (BeanNameUrlHandlerMapping)
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
