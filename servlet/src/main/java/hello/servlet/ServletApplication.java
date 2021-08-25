9ef68c97fd746e8190c88cb6839a0f7c34bd2c0f
jihunparkme <jihunpark.tech@gmail.com>
2021-08-25 21:36:09 +0900

    [스프링 MVC - 구조 이해] ViewResolver

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 /**
@@ -17,4 +20,10 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	/* application.properties 에 spring.mvc.view.prefix, spring.mvc.view.suffix 를 등록하지 않을 경우
+	@Bean
+	ViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/",".jsp");
+	}
+	*/
 }
