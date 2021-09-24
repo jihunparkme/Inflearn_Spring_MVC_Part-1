50373ad02fd3d92a809a0590b5ac2bdbf54b51f8
jihunparkme <jihunpark.tech@gmail.com>
2021-09-24 15:00:59 +0900

    [스프링 MVC - 웹 페이지 만들기] 상품 도메인 개발

---
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,44 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); //static 사용
+    private static long sequence = 0L; //static 사용
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    /**
+     * RequestBody로 Entity를 사용하는 것보다 DTO를 사용하는 것이 설계상 좋음.
+     *  - Entity 사용 시 id 변경 가능성..
+     */
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
